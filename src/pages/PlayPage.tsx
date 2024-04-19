b93ff2917b345c184574750e498a3410662fb7e0
unknown <pierremichel804@gmail.com>
2024-04-19 11:08:07 +0200

    V1.1- Fix taille des images

--- src/pages/PlayPage.tsx
+++ src/pages/PlayPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import { percent, px, viewHeight } from "csx";
 import { Colors } from "src/style/Colors";
 
@@ -114,174 +114,176 @@ export const PlayPage = () => {
   };
 
   return (
-    <Box
-      sx={{
-        width: percent(100),
-        height: viewHeight(100),
-        position: "relative",
-        display: "flex",
-      }}
-    >
+    <Container maxWidth="sm">
       <Box
         sx={{
+          width: percent(100),
+          height: viewHeight(100),
+          position: "relative",
           display: "flex",
-          flex: "1 1 0%",
-          flexDirection: "column",
         }}
       >
         <Box
           sx={{
-            flexGrow: 1,
             display: "flex",
+            flex: "1 1 0%",
             flexDirection: "column",
-            position: "relative",
           }}
         >
           <Box
             sx={{
-              backgroundColor: color1,
-              height: percent(50),
-              width: percent(100),
+              flexGrow: 1,
               display: "flex",
-              alignItems: "center",
-              gap: 2,
-              justifyContent: "center",
-              p: 2,
-              borderBottom: "2px solid white",
-              backgroundImage: value1 ? `url(${value1.image})` : "none",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              cursor: "pointer",
-              textAlign: "center",
-            }}
-            onClick={() => !vote && onVote(value1, value2)}
-          >
-            {value1 && (
-              <Box sx={{ display: "flex", gap: 2, flexDirection: "column" }}>
-                <JsonLanguageBlock
-                  variant="h1"
-                  sx={{
-                    color: Colors.white,
-                    textShadow: "2px 2px 4px black",
-                  }}
-                  value={value1.name}
-                />
-                {vote ? (
-                  <ResultBlock value={value1} />
-                ) : (
-                  <Box>
-                    <Button
-                      variant="contained"
-                      sx={{ borderRadius: px(50) }}
-                      onClick={(event) => {
-                        event.stopPropagation();
-                        dontKnow(value1);
-                      }}
-                    >
-                      <Typography variant="h6">
-                        {t("commun.notknow")}
-                      </Typography>
-                    </Button>
-                  </Box>
-                )}
-              </Box>
-            )}
-          </Box>
-          <Box
-            sx={{
-              position: "absolute",
-              top: percent(50),
-              left: percent(50),
-              translate: "-50% -55%",
-              borderRadius: percent(100),
-              m: 1,
-              border: `5px solid ${Colors.white}`,
+              flexDirection: "column",
+              position: "relative",
             }}
           >
             <Box
               sx={{
-                borderRadius: percent(50),
-                width: 80,
-                height: 80,
-                bgcolor: Colors.black,
+                backgroundColor: color1,
+                height: percent(50),
+                width: percent(100),
                 display: "flex",
-                justifyContent: "center",
                 alignItems: "center",
+                gap: 2,
+                justifyContent: "center",
+                p: 2,
+                borderBottom: "2px solid white",
+                backgroundImage: value1 ? `url(${value1.image})` : "none",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                cursor: "pointer",
+                textAlign: "center",
+              }}
+              onClick={() => !vote && onVote(value1, value2)}
+            >
+              {value1 && (
+                <Box sx={{ display: "flex", gap: 2, flexDirection: "column" }}>
+                  <JsonLanguageBlock
+                    variant="h1"
+                    sx={{
+                      color: Colors.white,
+                      textShadow: "2px 2px 4px black",
+                    }}
+                    value={value1.name}
+                  />
+                  {vote ? (
+                    <ResultBlock value={value1} />
+                  ) : (
+                    <Box>
+                      <Button
+                        variant="contained"
+                        sx={{ borderRadius: px(50) }}
+                        onClick={(event) => {
+                          event.stopPropagation();
+                          dontKnow(value1);
+                        }}
+                      >
+                        <Typography variant="h6">
+                          {t("commun.notknow")}
+                        </Typography>
+                      </Button>
+                    </Box>
+                  )}
+                </Box>
+              )}
+            </Box>
+            <Box
+              sx={{
+                position: "absolute",
+                top: percent(50),
+                left: percent(50),
+                translate: "-50% -55%",
+                borderRadius: percent(100),
+                m: 1,
+                border: `5px solid ${Colors.white}`,
               }}
             >
-              <BoltIcon sx={{ fontSize: 50, color: Colors.white }} />
+              <Box
+                sx={{
+                  borderRadius: percent(50),
+                  width: 80,
+                  height: 80,
+                  bgcolor: Colors.black,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <BoltIcon sx={{ fontSize: 50, color: Colors.white }} />
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                backgroundColor: color2,
+                height: percent(50),
+                width: percent(100),
+                display: "flex",
+                alignItems: "center",
+                gap: 2,
+                justifyContent: "center",
+                p: 2,
+                borderTop: "2px solid white",
+                backgroundImage: value2 ? `url(${value2.image})` : "none",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                cursor: "pointer",
+                textAlign: "center",
+              }}
+              onClick={() => !vote && onVote(value2, value1)}
+            >
+              {value2 && (
+                <Box sx={{ display: "flex", gap: 2, flexDirection: "column" }}>
+                  <JsonLanguageBlock
+                    variant="h1"
+                    sx={{
+                      color: Colors.white,
+                      textShadow: "2px 2px 4px black",
+                    }}
+                    value={value2.name}
+                  />
+                  {vote ? (
+                    <ResultBlock value={value2} />
+                  ) : (
+                    <Box>
+                      <Button
+                        variant="contained"
+                        sx={{ borderRadius: px(50) }}
+                        onClick={(event) => {
+                          event.stopPropagation();
+                          dontKnow(value2);
+                        }}
+                      >
+                        <Typography variant="h6">
+                          {t("commun.notknow")}
+                        </Typography>
+                      </Button>
+                    </Box>
+                  )}
+                </Box>
+              )}
             </Box>
           </Box>
-          <Box
-            sx={{
-              backgroundColor: color2,
-              height: percent(50),
-              width: percent(100),
-              display: "flex",
-              alignItems: "center",
-              gap: 2,
-              justifyContent: "center",
-              p: 2,
-              borderTop: "2px solid white",
-              backgroundImage: value2 ? `url(${value2.image})` : "none",
-              backgroundPosition: "center",
-              backgroundSize: "cover",
-              cursor: "pointer",
-              textAlign: "center",
-            }}
-            onClick={() => !vote && onVote(value2, value1)}
-          >
-            {value2 && (
-              <Box sx={{ display: "flex", gap: 2, flexDirection: "column" }}>
-                <JsonLanguageBlock
-                  variant="h1"
-                  sx={{
-                    color: Colors.white,
-                    textShadow: "2px 2px 4px black",
-                  }}
-                  value={value2.name}
-                />
-                {vote ? (
-                  <ResultBlock value={value2} />
-                ) : (
-                  <Box>
-                    <Button
-                      variant="contained"
-                      sx={{ borderRadius: px(50) }}
-                      onClick={(event) => {
-                        event.stopPropagation();
-                        dontKnow(value2);
-                      }}
-                    >
-                      <Typography variant="h6">
-                        {t("commun.notknow")}
-                      </Typography>
-                    </Button>
-                  </Box>
-                )}
-              </Box>
+          <Box sx={{ display: "flex", gap: 1, p: 1 }}>
+            <ButtonColor
+              value={Colors.red}
+              label={t("commun.quit")}
+              icon={CloseIcon}
+              variant="contained"
+              onClick={goToHome}
+            />
+            {vote && (
+              <ButtonColor
+                value={Colors.green}
+                label={t("commun.next")}
+                icon={SkipNextIcon}
+                variant="contained"
+                onClick={next}
+              />
             )}
           </Box>
         </Box>
-        <Box sx={{ display: "flex", gap: 1, p: 1 }}>
-          <ButtonColor
-            value={Colors.red}
-            label={t("commun.quit")}
-            icon={CloseIcon}
-            variant="contained"
-            onClick={goToHome}
-          />
-          {vote && (
-            <ButtonColor
-              value={Colors.green}
-              label={t("commun.next")}
-              icon={SkipNextIcon}
-              variant="contained"
-              onClick={next}
-            />
-          )}
-        </Box>
       </Box>
-    </Box>
+    </Container>
   );
 };
